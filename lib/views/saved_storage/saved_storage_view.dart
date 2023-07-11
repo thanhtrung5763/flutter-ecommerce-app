d2fdf4a62fab31652f4593bd6eabacaed256f74a
thanhtrung5763 <thanhtrung5763@gmail.com>
2023-07-11 20:13:58 +0700

    ui: create save storage view, fetch data and show

--- lib/views/saved_storage/saved_storage_view.dart
+++ lib/views/saved_storage/saved_storage_view.dart
@@ -2,7 +2,6 @@ import 'package:final_project/size_config.dart';
 import 'package:final_project/views/saved_storage/components/body.dart';
 import 'package:final_project/widgets/big_text.dart';
 import 'package:flutter/material.dart';
-import 'package:easy_localization/easy_localization.dart';
 
 class SavedStorageView extends StatelessWidget {
   const SavedStorageView({super.key});
@@ -11,14 +10,15 @@ class SavedStorageView extends StatelessWidget {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
+          centerTitle: true,
           title: BigText(
-            text: tr('SAVED ITEMS'),
+            text: 'SAVED ITEMS',
             size: 14,
           ),
-          elevation: 0.5,
         ),
-        body: SingleChildScrollView(
+        body: const SingleChildScrollView(
           child: Body(),
         ));
   }
