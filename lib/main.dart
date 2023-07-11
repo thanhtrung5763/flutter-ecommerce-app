94c5505a0decc100569322c03bdbcb8b76b7495b
thanhtrung5763 <thanhtrung5763@gmail.com>
2023-07-11 20:30:14 +0700

    app: main file

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,40 @@ import 'package:amplify_api/amplify_api.dart';
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_datastore/amplify_datastore.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:final_project/routers/app_router.dart';
 import 'package:final_project/models/ModelProvider.dart';
-import 'package:final_project/route_helper.dart';
 import 'package:final_project/services/auth/auth_navigator.dart';
 import 'package:final_project/services/auth/auth_repository.dart';
 import 'package:final_project/services/auth/cubit/auth_cubit.dart';
+import 'package:final_project/services/cloud/bloc/tracking_bloc.dart';
 import 'package:final_project/services/repo/user_repository.dart';
-import 'package:final_project/theme.dart';
+import 'package:final_project/utils/theme.dart';
 import 'package:final_project/views/home/cubit/session_cubit.dart';
 import 'package:final_project/views/main/main_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:easy_localization/easy_localization.dart';
+
 
 import 'amplifyconfiguration.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey =
+      'pk_test_51Lo1WDBGUwo1aZecZlLI32SNefJVQDYCU2y5C20qgWrcC7GGwSTxWEG0NKo3IvNRWf8kNzQWoKqub5ZQPiwxUzeY00pUw3kMpI';
+
   await EasyLocalization.ensureInitialized();
-  runApp(EasyLocalization(supportedLocales: const [Locale('en', 'US'), Locale('vi', 'VN')], path: 'assets/translations', fallbackLocale: const Locale('en', 'US'), child: const MyApp(),),);
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en', 'US'), Locale('vi', 'VN')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en', 'US'),
+      child: const MyApp(),
+    ),
+    // const MyApp(),
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -28,6 +43,10 @@ class MyApp extends StatefulWidget {
 
   @override
   State<MyApp> createState() => _MyAppState();
+
+  static void restartApp(BuildContext context) {
+    context.findAncestorStateOfType<_MyAppState>()!.restartApp();
+  }
 }
 
 class _MyAppState extends State<MyApp> {
@@ -38,37 +57,60 @@ class _MyAppState extends State<MyApp> {
     _configureAmplify();
   }
 
+  final _router = AppRouter();
+
+  Key key = UniqueKey();
+
+  void restartApp() {
+    setState(() {
+      key = UniqueKey();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: theme(),
-      home: _isAmplifyConfigured
-          ? MultiRepositoryProvider(
-              providers: [
-                RepositoryProvider(
-                  create: (context) => AuthRepository(),
-                ),
-                RepositoryProvider(
-                  create: (context) => UserRepository(),
-                ),
-              ],
-              child: BlocProvider(
-                create: (context) => SessionCubit(
-                  authRepository: context.read<AuthRepository>(),
-                  userRepository: context.read<UserRepository>(),
-                ),
-                child: AppNavigator(),
-              ),
-            )
-          : Scaffold(
-              body: Center(child: CircularProgressIndicator()),
-            ),
-      routes: RouteHelper.routes,
+    return KeyedSubtree(
+      key: key,
+      child: ScreenUtilInit(
+          // designSize: const Size(393, 851),
+          designSize: const Size(375, 812),
+          builder: (context, child) {
+            return MaterialApp(
+              localizationsDelegates: context.localizationDelegates,
+              supportedLocales: context.supportedLocales,
+              locale: context.locale,
+              title: 'Flutter Demo',
+              debugShowCheckedModeBanner: false,
+              theme: theme(),
+              home: _isAmplifyConfigured
+                  ? MultiRepositoryProvider(
+                      providers: [
+                        RepositoryProvider(
+                          create: (context) => AuthRepository(),
+                        ),
+                        RepositoryProvider(
+                          create: (context) => UserRepository(),
+                        ),
+                      ],
+                      child: BlocProvider(
+                        create: (context) => SessionCubit(
+                          authRepository: context.read<AuthRepository>(),
+                          userRepository: context.read<UserRepository>(),
+                        ),
+                        child: const AppNavigator(),
+                      ),
+                    )
+                  : const Scaffold(
+                      body: Center(child: CircularProgressIndicator()),
+                    ),
+              // routes: RouteHelper.routes,
+              // home: const StripePaymentScreen(),
+              // home: TestStripeApiScreen(),
+              // home: const MainTestView(),
+              // home: const TestSnackBar(),
+              // home: TestButtonSize(),
+            );
+          }),
     );
   }
 
@@ -106,14 +148,20 @@ class AppNavigator extends StatelessWidget {
           );
         } else if (state is Unauthenticated) {
           return BlocProvider(
-            create: (context) =>
-                AuthCubit(sessionCubit: context.read<SessionCubit>()),
+            create: (context) => AuthCubit(sessionCubit: context.read<SessionCubit>()),
             child: const AuthNavigator(),
           );
         } else if (state is Authenticated) {
-          return BlocProvider(
-            create: (context) =>
-                AuthCubit(sessionCubit: context.read<SessionCubit>()),
+          return MultiBlocProvider(
+            providers: [
+              BlocProvider(
+                create: (context) => AuthCubit(sessionCubit: context.read<SessionCubit>()),
+              ),
+              BlocProvider(
+                create: (context) =>
+                    state.isNewUser ? (TrackingBloc()..add(TrackingProductClearEvent())) : TrackingBloc(),
+              ),
+            ],
             child: const MainView(),
           );
         } else {
--- lib/route_helper.dart
+++ lib/route_helper.dart
@@ -1,6 +1,7 @@
 import 'package:final_project/views/confirm/confirmation_view.dart';
 import 'package:final_project/views/home/home_view.dart';
 import 'package:final_project/views/login/login_view.dart';
+import 'package:final_project/views/main/main_view.dart';
 import 'package:final_project/views/product/product_view.dart';
 import 'package:final_project/views/sign_up/sign_up_view.dart';
 import 'package:flutter/material.dart';
@@ -10,11 +11,13 @@ class RouteHelper {
   static const String signUpRoute = '/sign-up';
   static const String confirmationRoute = '/confirmation';
   static const String homeRoute = '/home';
+  static const String mainRoute = '/main';
   static const String productInfoRoute = '/product-info';
 
   static final Map<String, WidgetBuilder> routes = {
     loginRoute: (context) => const LoginView(),
     signUpRoute: (context) => const SignUpView(),
     homeRoute: (context) => const HomeView(),
+    mainRoute: (context) => const MainView(),
   };
 }
