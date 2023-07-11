fd0a1b531c8fa8cce2828ae47b2ed9e2dcb3d48e
thanhtrung5763 <thanhtrung5763@gmail.com>
2023-07-11 18:45:55 +0700

    config: setup constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -14,3 +14,5 @@ enum PopupMenuValue {
   similar_items,
   delete,
 }
+
+const SECRET_KEY = 'sk_test_51Lo1WDBGUwo1aZecKEfkBZIF4CW0Ruse5BnLUVwnzuglVfVc1usuCas1K0bD5TX477P5icBmNRIwyYs0Bv1WpfHn00n04G1umL';
